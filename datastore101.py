544cdb1a46b76b33fd9b2a58536e42436e161ec0
Sharif Salah <sharifsalah@Sharifs-MacBook-Pro.local>
2015-02-12 05:41:56 +0000

    Tidy imports
    Tidy of import statements and added PyCharm to .gitignore

--- datastore101.py
+++ datastore101.py
@@ -15,12 +15,13 @@
 # limitations under the License.
 #
 
-import jinja2
 import os
-import webapp2
 import datetime
 
+import jinja2
+import webapp2
 from google.appengine.ext import ndb
+
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
